49f4b73633c36c8203dc54c0b0f1b692f549c877
Victor Balena <victorbalenaes@gmail.com>
2025-09-12 15:19:00 -0300

    feat: adiciona controller, model e finaliza view de posts em painel de controle

--- layouts/admin/views/posts/index.blade.php
+++ layouts/admin/views/posts/index.blade.php
@@ -9,7 +9,32 @@
         <div class="alert alert-warning" role="alert">Você ainda não fez nenhum Post.</strong>
         </div>
     @else
-        @foreach ($posts as $post)
-        @endforeach
+        <div class="table-responsive">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Título</th>
+                        <th scope="col">Texto</th>
+                        <th scope="col">Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($posts as $post)
+                        <tr>
+                            <th scope="row">{{ $post->id }}</th>
+                            <td>{{ $post->titulo }}</td>
+                            <td class="">{{ $post->texto }}</td>
+                            <td>
+                                <i
+                                    class="{{ $post->status == 1 ? 'fa-solid fa-check text-primary' : 'fa-solid fa-close text-danger ' }}"></i>
+                            </td>
+
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+        <hr>
     @endif
 @endsection
