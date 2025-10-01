05a471804928b22c2b9e8d63e2bf2834e62bec1d
Victor Balena <victorbalenaes@gmail.com>
2025-10-01 09:22:35 -0300

    remove: remove breadcrumb do dashboard

--- layouts/admin/views/dashboard.blade.php
+++ layouts/admin/views/dashboard.blade.php
@@ -10,12 +10,6 @@
 @endsection
 
 @section('content')
-    <nav aria-label="breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item active" aria-current="page"><a
-                    href="{{ Helpers::url('/admin/dashboard') }}">Dashboard</a></li>
-        </ol>
-    </nav>
     <h5 class="mb-3">Seja bem-vindo de volta, {{ UsuarioController::usuario()->nome }}!</h5>
 
     </div>
