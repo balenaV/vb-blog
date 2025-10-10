b1cc90e9373a6d6b507f09ae57319f5c4a3a76da
Victor Balena <victorbalenaes@gmail.com>
2025-10-10 17:49:49 -0300

    feat: adiciona pasta Storage e classe Upload, para realizar uploads de arquivos

---
+++ app/Storage/Upload.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace sistema\Biblioteca;
+
+/**
+ * Classe Upload
+ *
+ *
+ */
+class Upload
+{
+    public $diretorio;
+    public $arquivo;
+    public $nome;
+    public $subDiretorio;
+
+
+    public function __construct(string $diretorio = null)
+    {
+        $this->diretorio = $diretorio ?? 'uploads';
+
+        if (!file_exists($this->diretorio) && !is_dir($this->diretorio)) {
+            mkdir($this->diretorio, 0755);
+        }
+    }
+
+    public function arquivo(array $arquivo, string $nome = null, string $subDiretorio = null)
+    {
+        $this->arquivo = $arquivo;
+
+        $this->nome = $nome ?? pathinfo($this->arquivo['name'], PATHINFO_FILENAME);
+
+        $this->subDiretorio = $subDiretorio ?? 'arquivos';
+
+        $this->criarSubDiretorio();
+        $this->renomarArquivo();
+        $this->moverArquivo();
+    }
+
+    public function criarSubDiretorio(): void
+    {
+        if (!file_exists($this->diretorio . DIRECTORY_SEPARATOR . $this->subDiretorio) && !is_dir($this->diretorio . DIRECTORY_SEPARATOR . $this->subDiretorio)) {
+            mkdir($this->diretorio . DIRECTORY_SEPARATOR . $this->subDiretorio, 0755);
+        }
+    }
+
+    public function renomarArquivo(): void
+    {
+        $arquivo = $this->nome . strrchr($this->arquivo['name'], '.');
+        if (file_exists($this->diretorio . DIRECTORY_SEPARATOR . $this->subDiretorio . DIRECTORY_SEPARATOR . $arquivo)) {
+            $arquivo = $this->nome . '-' . uniqid() . strrchr($this->arquivo['name'], '.');
+        }
+        $this->nome = $arquivo;
+    }
+
+    public function moverArquivo(): void
+    {
+        if (move_uploaded_file($this->arquivo['tmp_name'], $this->diretorio . DIRECTORY_SEPARATOR . $this->subDiretorio . DIRECTORY_SEPARATOR . $this->nome)) {
+            echo $this->nome . ' foi movido com sucesso';
+        } else {
+            echo 'Erro ao enviar arquivo';
+        }
+    }
+}
